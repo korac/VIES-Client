d21c0015eb835126d1bab816ea29e413083cd223
Kristijan Korać <korac.kristijan@gmail.com>
2016-12-07 00:15:10 +0100

    countrycode fix

--- js/services/VatService.js
+++ js/services/VatService.js
@@ -12,7 +12,8 @@
     this.requestVatInfo   = requestVatInfo;
 
     function requestVatInfo(vat){
-      return $http.get('https://morning-tundra-61517.herokuapp.com/vats/' + vat.number).then(
+      console.log(vat);
+      return $http.get('https://morning-tundra-61517.herokuapp.com/vats/' + vat.countryCode + vat.number).then(
         function successCallback(response){
           console.log("SUCCESS");
           return response.data;
